16100358dd0cfd707857ebad70b3ff91064a8ab6
dipendra-kumar <bhattdipak356@gmail.com>
2023-08-30 23:45:46 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,16 +1,41 @@
-# payment-app
+# TypeScriptMongoAuth
 
-This is the backend for the Payment Processing app 
+TypeScriptMongoAuth is a secure and versatile user authentication system built with TypeScript and MongoDB. It offers a range of features for managing user registration, login, email verification, password management, and secure password reset processes.
 
-## Quick Start
+## Key Features
 
-Get started developing...
+- **User Registration**: Allow users to sign up securely, providing essential information like username, email, and password.
+
+- **Email Verification**: Enhance security by verifying user emails through a confirmation link sent upon registration.
+
+- **Password Management**: Store user passwords securely using hashing and salting techniques.
+
+- **User Login**: Users can securely log in using their credentials, with password verification.
+
+- **Token-Based Authentication**: Utilizes JSON Web Tokens (JWT) for secure and efficient authentication after login.
+
+- **Password Reset**: Implement a secure process for users to reset their forgotten passwords via email.
+
+- **User Profile Management**: Users can update their profiles and account settings.
+
+- **Logging and Error Handling**: Includes comprehensive logging and error handling for monitoring system activities.
+
+- **Scalability and Performance**: Built to handle large user databases and high traffic loads, thanks to MongoDB's scalability.
+
+## Getting Started
+
+To get started with this template, clone the repo and follow the given instructions.
+
+## Installation
 
 # install dependencies
+
 npm install
 
 # run in development mode
+
 npm run dev
 
 # run tests
+
 npm run test
--- src/modules/v1/users/user.controller.ts
+++ src/modules/v1/users/user.controller.ts
@@ -2,11 +2,16 @@ import { Request, Response } from 'express';
 import { userModel } from 'src/models/users.model';
 import { createAuthToken } from 'src/helpers/jwt.helper';
 import { validateRegisterFields } from './userValidations';
-import { handleSuccessResponse, handleErrorResponse } from 'src/modules/utils/response.helper';
+import {
+  handleSuccessResponse,
+  handleErrorResponse,
+} from 'src/modules/utils/response.helper';
 import messages from 'src/constants/messages';
 import HttpStatus from 'src/constants/httpStatus';
-import { encryptPassword, matchUserPassword } from 'src/modules/utils/user.helper';
-import { sendMail } from 'src/helpers/mail.helper';
+import {
+  encryptPassword,
+  matchUserPassword,
+} from 'src/modules/utils/user.helper';
 
 export const checkAvailability = async (
   req: Request,
@@ -25,35 +30,71 @@ export const checkAvailability = async (
       handleErrorResponse(res, HttpStatus.BAD_REQUEST, existingMessage);
     }
   } catch (error) {
-
     handleErrorResponse(res, HttpStatus.INTERNAL_SERVER_ERROR, error.message);
   }
 };
 
 export const registerUser = async (req: Request, res: Response) => {
-  const { first_name, last_name, email, phone, password, address, zip_code, city, state } = req.body;
+  const {
+    first_name,
+    last_name,
+    email,
+    phone,
+    password,
+    address,
+    zip_code,
+    city,
+    state,
+  } = req.body;
   try {
     const errors = validateRegisterFields(req.body);
     if (errors.length > 0) {
-      return handleErrorResponse(res, HttpStatus.BAD_REQUEST, errors.join(", "));
+      return handleErrorResponse(
+        res,
+        HttpStatus.BAD_REQUEST,
+        errors.join(', ')
+      );
     }
 
     const userExist = await userModel.findOne({ $or: [{ email }, { phone }] });
     if (userExist) {
-      return handleErrorResponse(res, HttpStatus.BAD_REQUEST, messages.USER_ALREADY_EXISTS);
+      return handleErrorResponse(
+        res,
+        HttpStatus.BAD_REQUEST,
+        messages.USER_ALREADY_EXISTS
+      );
     }
 
     const newUser = new userModel({
-      first_name, last_name, email, phone, address, zip_code, city, state,
+      first_name,
+      last_name,
+      email,
+      phone,
+      address,
+      zip_code,
+      city,
+      state,
       password: await encryptPassword(password),
-      wallet_balance: 0
+      wallet_balance: 0,
     });
     await newUser.save();
 
-    const authToken = createAuthToken({ email: newUser.email, expires_in: "1h" });
-    return handleSuccessResponse(res, HttpStatus.OK, messages.REGISTRATION_SUCCESS, authToken);
+    const authToken = createAuthToken({
+      email: newUser.email,
+      expires_in: '1h',
+    });
+    return handleSuccessResponse(
+      res,
+      HttpStatus.OK,
+      messages.REGISTRATION_SUCCESS,
+      authToken
+    );
   } catch (error) {
-    return handleErrorResponse(res, HttpStatus.INTERNAL_SERVER_ERROR, error.message);
+    return handleErrorResponse(
+      res,
+      HttpStatus.INTERNAL_SERVER_ERROR,
+      error.message
+    );
   }
 };
 
@@ -61,36 +102,72 @@ export const getUserDetails = async (req: Request, res: Response) => {
   try {
     const userData = req.user;
     if (!userData) {
-      return handleErrorResponse(res, HttpStatus.BAD_REQUEST, messages.INVALID_EMAIL_FORMAT);
+      return handleErrorResponse(
+        res,
+        HttpStatus.BAD_REQUEST,
+        messages.INVALID_EMAIL_FORMAT
+      );
     }
-    return handleSuccessResponse(res, HttpStatus.OK, messages.DATA_FETCHED_SUCESSFULLY, userData);
+    return handleSuccessResponse(
+      res,
+      HttpStatus.OK,
+      messages.DATA_FETCHED_SUCESSFULLY,
+      userData
+    );
   } catch (error) {
-    return handleErrorResponse(res, HttpStatus.INTERNAL_SERVER_ERROR, error.message);
+    return handleErrorResponse(
+      res,
+      HttpStatus.INTERNAL_SERVER_ERROR,
+      error.message
+    );
   }
 };
 
 export const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  if (!email || !password) return handleErrorResponse(res, HttpStatus.BAD_REQUEST, messages.FIELDS_REQUIRED)
+  if (!email || !password)
+    return handleErrorResponse(
+      res,
+      HttpStatus.BAD_REQUEST,
+      messages.FIELDS_REQUIRED
+    );
 
   try {
     const userData = await userModel.findOne({ email: email });
     if (!userData) {
-      return handleErrorResponse(res, HttpStatus.FORBIDDEN, messages.USER_NOT_FOUND)
+      return handleErrorResponse(
+        res,
+        HttpStatus.FORBIDDEN,
+        messages.USER_NOT_FOUND
+      );
     }
 
-    const matchPassword = await matchUserPassword(userData.password, password)
+    const matchPassword = await matchUserPassword(userData.password, password);
     if (!matchPassword) {
-      return handleErrorResponse(res, HttpStatus.UNAUTHORIZED, messages.INVALID_CREDS)
+      return handleErrorResponse(
+        res,
+        HttpStatus.UNAUTHORIZED,
+        messages.INVALID_CREDS
+      );
     }
 
-    const authToken = createAuthToken({ email: userData.email, expires_in: "1h" });
+    const authToken = createAuthToken({
+      email: userData.email,
+      expires_in: '1h',
+    });
 
-
-    return handleSuccessResponse(res, HttpStatus.OK, messages.LOGIN_SUCCESS, authToken)
-
+    return handleSuccessResponse(
+      res,
+      HttpStatus.OK,
+      messages.LOGIN_SUCCESS,
+      authToken
+    );
   } catch (error) {
-    return handleErrorResponse(res, HttpStatus.INTERNAL_SERVER_ERROR, error.message);
+    return handleErrorResponse(
+      res,
+      HttpStatus.INTERNAL_SERVER_ERROR,
+      error.message
+    );
   }
-}
+};
