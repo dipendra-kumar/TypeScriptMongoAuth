fcefd3c21e954c84a24ec1590c2df767681d7e76
dipendra-kumar <bhattdipak356@gmail.com>
2023-08-31 00:03:08 +0530

    Updated app.ts and package.json

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import fs from 'fs'
-import https from 'https'
+import fs from 'fs';
+import https from 'https';
 import { createServer } from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -10,8 +10,8 @@ import morgan from 'morgan';
 import cors from 'cors';
 import logger from 'src/helpers/logger/logger';
 import { appRouter } from 'src/modules/app.route';
-import swaggerUi from 'swagger-ui-express'
-import swaggerDocs from 'src/swagger.json'
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from 'src/swagger.json';
 import 'src/config/db';
 import { handleScheduleTasks } from 'src/helpers/agenda/schedulers';
 
@@ -26,32 +26,22 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 var swaggerOptions = {
-  explorer: true
+  explorer: true,
 };
 app.use(morgan('dev'));
 
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, swaggerOptions))
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocs, swaggerOptions)
+);
 app.use('/uploads', express.static(`${__dirname}/uploads`));
 app.use('/api/v1', appRouter);
 
-const port = process.env.PORT ?? 6094;
+const port = process.env.PORT || 5000;
 
-let server: any;
-
-const currentDir = process.cwd();
-
-if (process.env.NODE_ENV !== "development") {
-  const privateKey = fs.readFileSync(`${currentDir}/ssl/privkey.pem`, "utf8");
-  const certificate = fs.readFileSync(`${currentDir}/ssl/fullchain.pem`, "utf8");
-  const credentials = { key: privateKey, cert: certificate };
-  server = https.createServer(credentials, app);
-} else {
-  server = createServer(app);
-}
-
-server.listen(port, () => {
-  logger.info("Server is running on " + port);
+app.listen(port, () => {
+  logger.info('Server is running on ' + port);
 });
-
